7578f7926fae799de337f2b2ab6a7d39b675a7dc
Chhavi Kankaria <ckankaria@pivotal.io>
2018-03-15 14:52:02 -0400

    tests are passing, backup before refactor

--- src/main/java/org/cloudfoundry/credhub/service/EncryptionProviderFactory.java
+++ src/main/java/org/cloudfoundry/credhub/service/EncryptionProviderFactory.java
@@ -16,7 +16,7 @@ public class EncryptionProviderFactory {
   private LunaProviderProperties lunaProviderProperties;
   private TimedRetry timedRetry;
   private PasswordKeyProxyFactory passwordKeyProxyFactory;
-  private HashMap<String, InternalEncryptionService> map;
+  private HashMap<String, EncryptionProvider> map;
 
   @Autowired
   public EncryptionProviderFactory(EncryptionKeysConfiguration keysConfiguration,
@@ -29,8 +29,9 @@ public class EncryptionProviderFactory {
     map = new HashMap<>();
   }
 
-  public InternalEncryptionService getEncryptionService(EncryptionKeyProvider provider) throws Exception {
-    InternalEncryptionService encryptionService;
+  public EncryptionProvider getEncryptionService(EncryptionKeyProvider provider) throws Exception {
+    EncryptionProvider encryptionService;
+
     if (map.containsKey(provider.getProviderName())) {
       return map.get(provider.getProviderName());
     } else {
@@ -39,6 +40,9 @@ public class EncryptionProviderFactory {
           encryptionService = new LunaEncryptionService(new LunaConnection(lunaProviderProperties),
               encryptionKeysConfiguration.isKeyCreationEnabled(),
               timedRetry);
+          break;
+        case EXTERNAL:
+          encryptionService = new ExternalEncryptionProvider(passwordKeyProxyFactory, provider.getHost(), Integer.valueOf(provider.getPort()));
           break;
         default:
           encryptionService = new PasswordEncryptionService(passwordKeyProxyFactory);
