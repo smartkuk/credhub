53b72416ccdd73dbfc128a483e19b044823b59b5
Guillermo Kardolus <gkardolus@pivotal.io>
2018-05-01 14:57:10 -0400

    WIP
    Signed-off-by: Mo Lambo <mlambo@pivotal.io>

--- src/main/java/org/cloudfoundry/credhub/service/PasswordEncryptionService.java
+++ src/main/java/org/cloudfoundry/credhub/service/PasswordEncryptionService.java
@@ -3,6 +3,8 @@ package org.cloudfoundry.credhub.service;
 import org.cloudfoundry.credhub.config.EncryptionKeyMetadata;
 import org.cloudfoundry.credhub.constants.CipherTypes;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.GCMParameterSpec;
